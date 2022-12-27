df797395d01f4bbb3a62fe57c3809428ff75eea2
artx <35874101+artx52135@users.noreply.github.com>
2022-12-27 21:47:29 +0000

    add shop page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index', as: 'home'
   get 'about' => 'pages#about', as: 'about'
+  get 'shop' => 'pages#shop', as: 'shop'
 
   resources :posts do
     resources :comments
