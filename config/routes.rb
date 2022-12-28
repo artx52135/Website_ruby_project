04fbbe65573da3f5dc9b9679c63f39ae44c74faf
artx <35874101+artx52135@users.noreply.github.com>
2022-12-28 11:45:38 +0000

    fix bag with log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'posts#index', as: 'home'
   get 'about' => 'pages#about', as: 'about'
   get 'shop' => 'pages#shop', as: 'shop'
